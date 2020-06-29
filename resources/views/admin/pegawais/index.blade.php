45e28e8850968f8adb5dfdd07db35e26e865c018
safroniaziz <safroni.aziz@gmail.com>
2020-06-29 11:33:40 +0700

    fx bug

--- resources/views/admin/pegawais/index.blade.php
+++ resources/views/admin/pegawais/index.blade.php
@@ -79,7 +79,7 @@
                                         <a href="{{ route('admin.pegawais.show',[$pegawai->slug]) }}" class="btn btn-primary btn-sm pr-3 pl-3" style="color:white;cursor:pointer;"><i class="fa fa-info"></i></a>
                                     </td>
                                     <td>
-                                        <a href="{{  }}" class="btn btn-sm btn-info text-white" style="cursor: pointer;"> <i class="fa fa-edit"></i>&nbsp;</a>
+                                        <a href="" class="btn btn-sm btn-info text-white" style="cursor: pointer;"> <i class="fa fa-edit"></i>&nbsp;</a>
                                     </td>
                                 </tr>
                             @endforeach
